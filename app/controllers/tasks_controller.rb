599df449ac72da8a8e45a6846472e024f141e89b
wan.wa <wwa602420@gmail.com>
2020-11-28 09:10:18 +0000

    CRUD tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,7 +12,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(message_params)
+    @task = Task.new(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
@@ -46,12 +46,14 @@ class TasksController < ApplicationController
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
   end
-
-end
-
+  
   private
 
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content)
   end
+
+end
+
+  
